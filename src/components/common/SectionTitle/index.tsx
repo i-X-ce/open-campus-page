a919ac249292e67a1d661f02c1d1e3d106a8efef
i-X-ce <iceci2804@gmail.com>
2025-06-30 01:13:52 +0900

    💫 SectionTitleのアニメーション

--- src/components/common/SectionTitle/index.tsx
+++ src/components/common/SectionTitle/index.tsx
@@ -1,4 +1,6 @@
+import { motion } from "motion/react";
 import styles from "./style.module.css";
+import { sectionInView } from "../CommonSection";
 
 const SectionTitle = ({
   title,
@@ -10,13 +12,40 @@ const SectionTitle = ({
   return (
     <div className={styles.container}>
       {Array.from({ length: 2 }, (_, i) => (
-        <div
-          className={`${styles.titleContainer} ${i === 1 ? styles.titleContainer1 : ""}`}
+        <motion.div
+          className={`${styles.titleContainer} ${
+            i === 1 ? styles.titleContainer1 : ""
+          }`}
           key={i}
+          initial={i === 0 ? { opacity: 0 } : { width: 0, paddingRight: 0 }}
+          variants={{
+            [sectionInView]:
+              i === 0
+                ? { opacity: 1, transition: { duration: 0.5 } }
+                : {
+                    width: "auto",
+                    paddingRight: 100,
+                    transition: { duration: 0.5, delay: 0.7 },
+                  },
+          }}
         >
-          <p className={styles.subTitle}>{subTitle}</p>
-          <p className={`${styles.title} big-s`}>{title}</p>
-        </div>
+          <motion.p
+            initial={{ opacity: 0, x: -50 }}
+            variants={{ [sectionInView]: { opacity: 1, x: 0 } }}
+            transition={{ delay: 0.6 }}
+            className={styles.subTitle}
+          >
+            {subTitle}
+          </motion.p>
+          <motion.p
+            initial={{ opacity: 0, x: -50 }}
+            variants={{ [sectionInView]: { opacity: 1, x: 0 } }}
+            transition={{ delay: 0.5 }}
+            className={`${styles.title} big-s`}
+          >
+            {title}
+          </motion.p>
+        </motion.div>
       ))}
     </div>
   );
