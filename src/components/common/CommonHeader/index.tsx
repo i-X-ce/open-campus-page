769f883155e9687309ebbf59b1bc801bedf5098b
i-X-ce <iceci2804@gmail.com>
2025-06-30 19:43:38 +0900

    📱 ヘッダーのレスポンシブ対応

--- src/components/common/CommonHeader/index.tsx
+++ src/components/common/CommonHeader/index.tsx
@@ -1,5 +1,8 @@
 import styles from "./style.module.css";
 import CUTRogo from "../CUTRogo";
+import { useState } from "react";
+import { CiMenuBurger } from "react-icons/ci";
+import { VscClose } from "react-icons/vsc";
 
 const LINKS = [
   "Program",
@@ -11,19 +14,42 @@ const LINKS = [
 ];
 
 const CommonHeader = () => {
+  const [open, setOpen] = useState(false);
+  const content = (
+    <ul className={`${styles.navList} big-s`}>
+      {LINKS.map((link) => (
+        <li key={link} className={styles.navItem}>
+          <a href={`#${link.toLowerCase().toLowerCase()}-section`}>
+            <p className={styles.linkText}>{link}</p>
+          </a>
+        </li>
+      ))}
+    </ul>
+  );
+
   return (
     <header className={styles.header}>
       <div className={styles.container}>
         <CUTRogo variant="header" />
-        <ul className={`${styles.navList} big-s`}>
-          {LINKS.map((link) => (
-            <li key={link} className={styles.navItem}>
-              <a href={`#${link.toLowerCase().toLowerCase()}-section`}>
-                <p className={styles.linkText}>{link}</p>
-              </a>
-            </li>
-          ))}
-        </ul>
+        {content}
+      </div>
+      <div className={styles.smallContainer}>
+        <div
+          className={styles.toggleButton}
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          {open ? <VscClose /> : <CiMenuBurger />}
+        </div>
+        {open && (
+          <div className={styles.smallMenu}>
+            <div className={styles.smallRogoContainer}>
+              <CUTRogo variant="footer" />
+            </div>
+            {content}
+          </div>
+        )}
       </div>
     </header>
   );
