a01ffb2a84e847532eeb24e5790fdbfc7df65c42
i-X-ce <iceci2804@gmail.com>
2025-07-01 19:25:46 +0900

    📱 フッターのレスポンシブ対応

--- src/components/common/CommonFooterLinks/index.tsx
+++ src/components/common/CommonFooterLinks/index.tsx
@@ -1,4 +1,6 @@
+import { useState } from "react";
 import styles from "./style.module.css";
+import { VscChevronDown } from "react-icons/vsc";
 
 const CommonFooterLinks = ({
   title,
@@ -10,27 +12,37 @@ const CommonFooterLinks = ({
     href: string;
   }[];
 }) => {
+  const [open, setOpen] = useState(false);
+
+  const linkContent = (
+    <div className={styles.links}>
+      {links.map((link, i) => (
+        <div key={i} className={styles.link}>
+          <div className={styles.linkBorder} />
+          <a href={link.href} className={styles.linkA}>
+            {link.name}
+          </a>
+        </div>
+      ))}
+      <div className={styles.linkEnd}>
+        <div className={styles.linkEndBorder} />
+        <div className={styles.linkEndPoint}></div>
+      </div>
+    </div>
+  );
+
   return (
     <div className={styles.container}>
-      <p className={styles.title}>{title}</p>
+      <button className={styles.titleButton} onClick={() => setOpen(!open)}>
+        <p className={styles.title}>{title}</p>
+        <VscChevronDown className={styles.downButton} />
+      </button>
       <div className={styles.linksContainer}>
         <div className={styles.linksBorderWrapper}>
           <div className={styles.linksBorder} />
         </div>
-        <div className={styles.links}>
-          {links.map((link, i) => (
-            <div key={i} className={styles.link}>
-              <div className={styles.linkBorder} />
-              <a href={link.href} className={styles.linkA}>
-                {link.name}
-              </a>
-            </div>
-          ))}
-          <div className={styles.linkEnd}>
-            <div className={styles.linkEndBorder} />
-            <div className={styles.linkEndPoint}></div>
-          </div>
-        </div>
+        <div className={styles.linkWrapper}>{linkContent}</div>
+        <div className={styles.linkWrapperSmall}>{open && linkContent}</div>
       </div>
     </div>
   );
