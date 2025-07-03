9bea2f3f114a21e93876d75ee24e8cc60e22521b
kiiiiiiiiiiiiiiii <kiichi20040428@gmail.com>
2025-07-03 14:19:02 +0900

    💫 HeloSectionのアニメーション

--- src/components/opencampus/HeloSection/index.tsx
+++ src/components/opencampus/HeloSection/index.tsx
@@ -1,18 +1,62 @@
+import { motion } from "motion/react";
 import styles from "./style.module.css";
+import { sectionInView } from "../../common/CommonSection";
+
+const kEY_FRAME = {
+  line: .3,
+  text: .5,
+}
 
 const HeloSection = () => {
   return (
-    <section className={styles.section} id="hero-section">
+    <motion.section className={styles.section} id="hero-section"
+      whileInView={sectionInView}
+      viewport={{ once: true, amount: 0.5 }}
+    >
       <div className={styles.container}>
         <div className={`${styles.textContainer} big-s`}>
-          <div className={styles.leftLine} />
-          <div className={styles.leftLine2} />
+          <div className={styles.lineWrapper1}>
+            <motion.div className={styles.leftLine}
+              initial={{ width: "50vw" }}
+              variants={{
+                sectionInView: { width: "100%", }
+              }}
+              transition={{ duration: 0.7, delay: kEY_FRAME.line, ease: "circOut" }}
+            />
+          </div>
+          <div className={styles.lineWrapper2}>
+            <motion.div className={styles.leftLine2}
+              initial={{ height: "70vh" }}
+              variants={{
+                sectionInView: { height: "100%", }
+              }}
+              transition={{ duration: 0.6, delay: 0.5 + kEY_FRAME.line, ease: "circOut" }}
+            />
+          </div>
           <div>
-            <p>OPEN</p>
-            <p>CAMPUS</p>
-            <div className={styles.date}>
-              2025 <div className={styles.day}>8/12-15</div>
-            </div>
+            <motion.p
+              initial={{ opacity: 0, x: 100 }}
+              variants={{ sectionInView: { opacity: 1, x: 0 } }}
+              transition={{ duration: 1.5, delay: kEY_FRAME.text }}
+            >OPEN</motion.p>
+            <motion.p
+              initial={{ opacity: 0, x: 100 }}
+              variants={{ sectionInView: { opacity: 1, x: 0 } }}
+              transition={{ duration: 1.5, delay: 0.3 + kEY_FRAME.text }}
+            >CAMPUS</motion.p>
+            <motion.div className={styles.date}
+              initial={{ opacity: 0, y: 20 }}
+              variants={{ sectionInView: { opacity: 1, y: 0 } }}
+              transition={{ duration: 1.5, delay: 0.5 + kEY_FRAME.text }}
+            >
+              2025 <motion.div className={styles.day}>8/12-15
+                <motion.div className={styles.dayBack}
+                  initial={{ width: "100%" }}
+                  variants={{ sectionInView: { width: "0%" } }}
+                  transition={{ duration: 1, delay: 1 + kEY_FRAME.text, ease: "circOut" }}
+                />
+              </motion.div>
+            </motion.div>
           </div>
         </div>
         {/* <div className={styles.windowContainer}>
@@ -32,7 +76,7 @@ const HeloSection = () => {
           </div>
         </div>
       </div>
-    </section>
+    </motion.section>
   );
 };
 
